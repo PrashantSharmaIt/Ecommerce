a59f1d1bc3c4ab4a8f376f452ad602ad6d571847
PrashantSharmaIt <sharmaprashant5771@gmail.com>
2024-09-05 13:56:20 +0530

    path changes

--- ecom/server.js
+++ ecom/server.js
@@ -7,7 +7,14 @@ import authRoutes from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoute.js'
 import cors from "cors";
 import productRoutes from './routes/productRoute.js'
-//import path from 'path'
+import path from 'path'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 //config env
 dotenv.config();
@@ -21,18 +28,18 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(morgan('dev'));
-//app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/category',categoryRoutes);
 app.use("/api/v1/product",productRoutes)
 
-app.get('/',(req,res)=>{
-  res.send("<h1>Welcome To Ecommerce Website</h1>")
-});
-// app.use('*',function(req,res){
-//   res.sendFile(path.join(__dirname, './client/build/index.html'));
-// })
+// app.get('/',(req,res)=>{
+//   res.send("<h1>Welcome To Ecommerce Website</h1>")
+// });
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+})
 
 const PORT = process.env.PORT || 8080;
 
